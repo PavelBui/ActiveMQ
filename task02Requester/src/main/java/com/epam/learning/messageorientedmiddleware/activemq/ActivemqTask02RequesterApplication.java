26d9fb8785017f56370733bf0c47429b3aa61c42
PavelBui <89442856+PavelBui@users.noreply.github.com>
2023-08-31 10:56:23 +0300

    Updated task 2

--- task02Requester/src/main/java/com/epam/learning/messageorientedmiddleware/activemq/ActivemqTask02RequesterApplication.java
+++ task02Requester/src/main/java/com/epam/learning/messageorientedmiddleware/activemq/ActivemqTask02RequesterApplication.java
@@ -1,28 +1,58 @@
 package com.epam.learning.messageorientedmiddleware.activemq;
 
 import com.epam.learning.messageorientedmiddleware.activemq.service.RequestService;
-import com.epam.learning.messageorientedmiddleware.activemq.service.ResponseService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.List;
+
+import static java.lang.Thread.sleep;
+
 @SpringBootApplication
 public class ActivemqTask02RequesterApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(ActivemqTask02RequesterApplication.class, args);
 		ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(ActivemqTask02RequesterApplication.class, args);
 
 		RequestService requestService = configurableApplicationContext.getBean(RequestService.class);
-		ResponseService responseService = configurableApplicationContext.getBean(ResponseService.class);
 
-		requestService.sendMessage("requestChanel", "Initial request");
-		boolean readyForRequest = false;
-
-		for (int i = 1; i < 11; i++) {
-			requestService.sendMessage("requestChanel", "Request " + i);
-		}
-
+		requestService.addMessage("Initial request");
+		requestService.addMessages(
+				List.of("Request 1",
+						"Request 2",
+						"Request 3",
+						"Request 4",
+						"Request 5",
+						"Request 6",
+						"Request 7",
+						"Request 8",
+						"Request 9",
+						"Request 10"));
+		requestService.sendNextMessage(RequestService.REQUEST_CHANEL);
+		requestService.addMessages(
+				List.of("Request 11",
+						"Request 12",
+						"Request 13",
+						"Request 14",
+						"Request 15",
+						"Request 16",
+						"Request 17",
+						"Request 18",
+						"Request 19",
+						"Request 20"));
+		sleep(2000);
+		requestService.addMessages(
+				List.of("Request 21",
+						"Request 22",
+						"Request 23",
+						"Request 24",
+						"Request 25",
+						"Request 26",
+						"Request 27",
+						"Request 28",
+						"Request 29",
+						"Request 30"));
 	}
-
 }
--- task02Requester/src/main/java/com/epam/learning/messageorientedmiddleware/activemq/service/RequestService.java
+++ task02Requester/src/main/java/com/epam/learning/messageorientedmiddleware/activemq/service/RequestService.java
@@ -1,9 +1,16 @@
 package com.epam.learning.messageorientedmiddleware.activemq.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import static java.lang.Thread.sleep;
 
 @Service
 public class RequestService {
@@ -11,8 +18,41 @@ public class RequestService {
     @Autowired
     private JmsTemplate jmsTemplate;
 
+    public static final String REQUEST_CHANEL = "requestChanel";
+    private static final String REPLY_CHANEL = "replyChanel";
+    private static final int QUEUE_ASKING_TIMEOUT_MS = 5000;
+    private static final int QUEUE_ASKING_LIMIT = 10;
+    private static Queue<String> messageQueue = new LinkedList<>();
+    private static int queueAskingIterator = 1;
+
     @Transactional
-    public void sendMessage(String destination, String message) {
-        jmsTemplate.convertAndSend(destination, message);
+    public void sendNextMessage(String destination) throws InterruptedException {
+        if (!messageQueue.isEmpty()) {
+            jmsTemplate.convertAndSend(destination, messageQueue.poll());
+            queueAskingIterator = 1;
+        } else {
+            if (queueAskingIterator > QUEUE_ASKING_LIMIT) {
+                System.out.println("Queue of messages is empty. I'm have asked a lot of the times. Needs manual event");
+            } else {
+                System.out.println("Queue of messages is empty. This is my " + queueAskingIterator + "'th ask. I'm going to sleep " + QUEUE_ASKING_TIMEOUT_MS + "ms");
+                sleep(QUEUE_ASKING_TIMEOUT_MS);
+                queueAskingIterator++;
+                sendNextMessage(REQUEST_CHANEL);
+            }
+        }
+    }
+
+    @JmsListener(destination = REPLY_CHANEL)
+    public void receiveMessage(String message) throws InterruptedException {
+        System.out.println("Requester received next message: " + message);
+        sendNextMessage(REQUEST_CHANEL);
+    }
+
+    public void addMessage(String message) {
+        messageQueue.add(message);
+    }
+
+    public void addMessages(List<String> messages) {
+        messageQueue.addAll(messages);
     }
 }
